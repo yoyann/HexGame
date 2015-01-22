c1a5df8664531c4a62b042f3e2c2426808ec709e
totocap <totocap@hotmail.fr>
2015-01-22 18:24:41 +0100

    Modifications sur l'interface HexModel (rajouts de méthodes, corrections mineures)

--- model/hexModel.java
+++ model/hexModel.java
@@ -5,53 +5,59 @@ import java.util.Set;
 import hexGame.util.Coord;
 
 /** 
- * @inv
- * 		getPlayer() != null
- * 		Soit c = getNextMoveAI() 
- * 			c != null
- * 			isFreeTile(c)
+ * @inv <pre>
+ * 		MIN_SIZE_BOARD <= getSize() <= MAX_SIZE_BOARD
+ *		getNextMoveAI() != null
+ * 		getPlayer() != null 
+ *		isFreeTile(getNextMoveAI())
+ * 		isFinished() <=> Il existe un unique PlayerId p tel que hasPlayerWon(p)
+ * 		getPositions(p) != null
+ * </pre>
  * 		
  * @cons
- * 		Un modele avec un plateau de jeu de hex.
+ * 		Construit un modele representant un jeu de hex fournit avec une IA.
  * 		$ARGS$ /
  * 		$PRE$ /
- * 		$POST$ Un plateau vide de taille MAX_SIZE_BOARD
+ * 		$POST$ <pre>
+ * 			getSize() == MAX_SIZE_BOARD
+ * 			forall Coord c : (0, 0) .. (getSize() - 1, getSize() - 1) : 
+ * 				isFreeTile(c)</pre>
  */
-public interface hexModel {
+public interface HexModel {
+	// ATTRIBUTS
+	
 	/**
-	 * La taille maximale du plateau.
-	 */
-	public static final int MAX_SIZE_BOARD = 11;
-	/**
-	 * La taille minimale du plateau.
+	 * La taille minimale d'un plateau de jeu.
 	 */
 	public static final int MIN_SIZE_BOARD = 4;
+	/**
+	 * La taille maximale d'un plateau de jeu.
+	 */
+	public static final int MAX_SIZE_BOARD = 11;
 	
 	// REQUETES
 	
 	/**
-	 * Calcule le prochain mouvement et la renvoie.
-	 * @pre
-	 * 		!isFinished()
+	 * Calcule le prochain mouvement que jouerait l'IA et le renvoie.
+	 * @pre <pre>
+	 * 		!isFinished()</pre>
 	 */
 	Coord getNextMoveAI();
 	
 	/**
-	 * La taille du plateau.
+	 * Retourne la taille du plateau.
 	 */
 	int getSize();
 	
 	/**
-	 * Le prochain joueur à jouer. 
-	 * @pre
-	 * 		!isFinished()
+	 * Retourne le prochain joueur a jouer.
 	 */
 	PlayerId getPlayer();
 	
 	/**
 	 * Teste si la case en c est vide.
-	 * @pre
-	 * 		c != null
+	 * @pre <pre>
+	 * 		c != null</pre>
 	 */
 	boolean isFreeTile(Coord c);
 	
@@ -61,42 +67,47 @@ public interface hexModel {
 	boolean isFinished();
 	
 	/**
-	 * Renvoie la liste des pièces jouées par joueur.
-	 * @pre
-	 * 		joueur != null
+	 * Teste si le joueur p a gagne la partie.
+	 * @pre <pre>
+	 * 		p != null</pre>
 	 */
-	Set<Coord> getPositions(PlayerId joueur); 
+	boolean hasPlayerWon(PlayerId p);
+	
+	/**
+	 * Renvoie la liste des coordonnées où joueur a des pièces.
+	 * @pre <pre>
+	 * 		p != null</pre>
+	 */
+	Set<Coord> getPositions(PlayerId p); 
 	
 	// COMMANDES
 	
 	/**
-	 * Joue le prochain mouvement en c.
-	 * @pre
+	 * Joue pour le joueur getPlayer() un mouvement en c.
+	 * @pre <pre>
 	 * 		c != null
 	 * 		isFreeTile(c)
-	 * 		!isFinished()
-	 * @post
-	 * 		old getPlayer() a joué en c
+	 * 		!isFinished()</pre>
+	 * @post <pre>
+	 * 		getPlayer() != old getPlayer()
+	 * 		c in getPositions(old getPlayer())</pre>
 	 */
 	void nextMove(Coord c);
 	
 	/**
-	 * Reset le jeu.
+	 * Vide le plateau de jeu.
+	 * @post <pre>
+	 * 		forall Coord c : (0, 0) .. (getSize() - 1, getSize() - 1) : 
+ * 				isFreeTile(c)</pre>
 	 */
 	void reset();
 	
 	/**
-	 * Joue le prochain mouvement calculé par l'IA.
-	 * @pre
-	 * 		!isFinished()
-	 */
-	void nextMoveAI();
-	
-	/**
-	 * Définit la nouvelle taille du tableau.
-	 * @pre
-	 * 		MIN_SIZE_BOARD <= size <= MAX_SIZE_BOARD
+	 * Défini la nouvelle taille du plateau de jeu.
+	 * @pre <pre>
+	 * 		MIN_SIZE_BOARD <= size <= MAX_SIZE_BOARD </pre>
+	 * @post <pre>
+	 * 		getSize() == size</pre>
 	 */
 	void setSize(int size);
-
 }
